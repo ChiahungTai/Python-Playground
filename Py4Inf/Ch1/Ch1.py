7589b39247e200a45713059299fc05cd6223829e
CTai <ctai at mozilla.com>
2015-04-09 10:50:43 +0800

    Ch2

--- Py4Inf/Ch1/Ch1.py
+++ Py4Inf/Ch1/Ch1.py
@@ -1,1 +1,37 @@
 print('Hello World')
+x = 100
+y = x + 50
+print(y)
+x = 5 
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+print('Fins')
+
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print("Blastoff")
+
+x = 1 + 2 ** 3 / 4 * 5
+print(x)
+
+print(9/2)
+
+eee="WTF"
+print(type(eee))
+
+xx = 2
+print(type(xx))
+yyy = 3.5
+print(type(yyy))
+zzz=99/100
+print(type(zzz))
+
+sval = int('123')
+print(sval + 1)
+
+name = 'ctai'
+print("Welcome ", name)
